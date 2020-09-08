2ddceb8a494747395dff8119956865a2de5b4990
mas rosyid <rosyidgrobogan@gmail.com>
2020-09-08 13:07:50 +0700

    Updates

---
+++ precache-manifest.543d4d53c08d5738b821ecc6877cd045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b572b3a75c1fbdb2407909069d607833",
+    "url": "/index.html"
+  },
+  {
+    "revision": "356cfe6986da3e86ad12",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "356cfe6986da3e86ad12",
+    "url": "/static/js/main.a828314b.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
